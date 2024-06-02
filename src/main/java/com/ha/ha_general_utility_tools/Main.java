eca5f41b54b241c982965ba754c8f6227e7e9508
hasiabewardana <hasi.prasanga@gmail.com>
2024-06-02 15:36:34 +0530

    Updated the source.

--- src/main/java/com/ha/ha_general_utility_tools/Main.java
+++ src/main/java/com/ha/ha_general_utility_tools/Main.java
@@ -1,4 +1,4 @@
-package com.example.ha_general_utility_tools;
+package com.ha.ha_general_utility_tools;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
