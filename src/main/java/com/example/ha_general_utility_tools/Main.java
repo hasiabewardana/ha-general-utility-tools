eba38d56869eb3e6b1f1bc5554ee32d54af7791f
hasiabewardana <hasi.prasanga@gmail.com>
2024-06-02 10:00:51 +0530

    Updated the source.

--- src/main/java/com/example/ha_general_utility_tools/Main.java
+++ src/main/java/com/example/ha_general_utility_tools/Main.java
@@ -7,5 +7,6 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello and welcome to HA General Tools!\n");
+
     }
 }
